8edaf00a6f65eee55f1bfa704b64c75c58f82303
habeebullahokunlola@gmail.com <habeebullahokunlola@gmail.com>
2023-08-03 00:16:11 +0100

    commit two

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 
 const head = (
     <nav>
-        <h1>my name Habeebullah</h1>
-        <h2>a student of Osun state </h2>
-        <h5>I love coding</h5>
+        <h1>My name Habeebullah and this is my first react project</h1>
+        <h2>Fun facts about react</h2>
+        <ul>
+            <li>First released in 2013</li>
+            <li>Created by Jordan Walke</li>
+            <li>Maintained by Facebook</li>
+            <li>A Javascript library</li>
+        </ul>
     </nav>
     
 )
